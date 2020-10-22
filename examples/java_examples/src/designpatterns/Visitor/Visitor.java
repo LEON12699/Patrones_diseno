1601b0b1c6bfc596aa8624bca58f6f40d7140432
leon12699 <israelpat42@gmail.com>
2020-10-22 17:09:26 -0500

    visitor

--- examples/java_examples/src/designpatterns/Visitor/Visitor.java
+++ examples/java_examples/src/designpatterns/Visitor/Visitor.java
@@ -2,6 +2,11 @@ package designpatterns.Visitor;
 
 
 public interface Visitor {
-	
-	
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
 }
